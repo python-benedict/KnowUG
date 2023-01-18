3a6fdf1aae422cb4d6adbf81f4c5f73d9f75bc61
Benedict <newlyf38@gmail.com>
2023-01-18 10:23:43 +0000

    added a safeareaview so that all the content move from the status or nav bar to the start of the screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 
 export default function App() {
   return (
     <TailwindProvider>
-      <View>
+      <SafeAreaView>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      </SafeAreaView>
     </TailwindProvider>
   );
 }
